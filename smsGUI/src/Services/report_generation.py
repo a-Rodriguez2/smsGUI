39099b231b518f5c9f4a45e304b09534e5ca9224
Colby Ruane <125488077+ColbyRuane@users.noreply.github.com>
2024-04-11 15:11:41 -0500

    Update Report Generation

--- smsGUI/src/Services/report_generation.py
+++ smsGUI/src/Services/report_generation.py
@@ -1,18 +1,23 @@
 import PyPDF2
 from PyQt5.QtWidgets import QFileDialog
-import getpass
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from datetime import datetime
 
 
-def create_pdf_with_image(image_path1, image_path2, save_report):
+def create_pdf_with_image(image_path1, image_path2, save_report,
+                          ppid, current_user, name, worker_id, station_number):
     """
 
-    :param image_path1: result of image comparison
-    :param image_path2: current DUT (device under test)
-    :param save_report: decides whether 'save report' or 'save as report' operation
-    :return: NoneType
+    :param image_path1: Image of result comparison.
+    :param image_path2: Image of Motherboard Unit Under Test.
+    :param save_report: Decides whether 'save' or 'save as'.
+    :param ppid: Motherboard identifier.
+    :param current_user: OS system username.
+    :param name: Name decided by user.
+    :param worker_id: Worker ID decided by user.
+    :param station_number: Station Number decided by user.
+    :return: None
 
     """
 
@@ -23,9 +28,6 @@ def create_pdf_with_image(image_path1, image_path2, save_report):
     current_time = datetime.now()
     current_time_name = current_time.strftime('%Y-%m-%d_%H_%M_%S')
     current_time_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
-
-    # determine system username
-    current_user = getpass.getuser()
 
     # report 'save' operation
     if save_report == 1:
@@ -46,45 +48,51 @@ def create_pdf_with_image(image_path1, image_path2, save_report):
         # Create a canvas object to draw on the PDF
         c = canvas.Canvas(output_filename, pagesize=letter)
 
-        # Calculate x-coordinate for centering
-        text_width1 = c.stringWidth("Report Results")
+        # Determine coordinates for text and images
+        text1 = f'Report Results:  {current_time_str}'
+        text_width1 = c.stringWidth(text1)
         x_coordinate1 = (letter[0] - text_width1) / 2
+        c.drawString(x_coordinate1, 760, text1)
 
-        text_width2 = c.stringWidth("Motherboard Under Test")
-        x_coordinate2 = (letter[0] - text_width2) / 2
+        text2 = f'Unit Under Test: {ppid}'
+        text_width2 = c.stringWidth(text2)
+        x_coordinate2 = (letter[0] - 480) / 2
+        c.drawString(x_coordinate2, 730, text2)
 
-        text_width3 = c.stringWidth("Current Time: " + current_time_str)
+        text3 = f'Current User: {current_user} ({name})'
+        text_width3 = c.stringWidth(text3)
         x_coordinate3 = (letter[0] - text_width3) / 2
+        c.drawString(x_coordinate2, 710, text3)
 
-        text_width4 = c.stringWidth("Current User: " + current_user)
-        x_coordinate4 = (letter[0] - text_width4) / 2
+        text4 = f'Worker ID: {worker_id}'
+        text_width4 = c.stringWidth(text4)
+        c.drawString(x_coordinate2, 690, text4)
 
-        # Write "Report Results" at the top centered horizontally
-        c.drawString(x_coordinate1, 750, "Report Results")
-        c.drawString(x_coordinate2, 730, "Unit Under Test")
-        c.drawString(x_coordinate3, 710, "Current Time: " + current_time_str)
-        c.drawString(x_coordinate4, 690, "Current User: " + current_user)
+        text5 = f'Station Number: {station_number}'
+        text_width5 = c.stringWidth(text5)
+        c.drawString(x_coordinate2, 670, text5)
 
-        # Calculate x-coordinate for centering images
-        image_width1 = 480  # Assuming the width of images is 320
+        # Define image dimensions before writing to pdf
+        image_width = 420
+        # 16:9 aspect ratio
+        image_height = (image_width * 0.75)
+        image_x_coordinate = (letter[0] - image_width) / 2
 
-        image_width2 = 240
+        c.drawImage(image_path1, image_x_coordinate, 340, width=image_width, height=image_height)
+        c.drawImage(image_path2, image_x_coordinate, 10, width=image_width, height=image_height)
 
-        image_x_coordinate = (letter[0] - image_width1) / 2
+        text6 = 'Comparison Result'
+        text_width6 = c.stringWidth(text6)
+        x_coordinate6 = (letter[0] - text_width6) / 2
+        c.drawString(x_coordinate6, 350, text6)
 
-        # Insert the images onto the canvas, centered horizontally
-        c.drawImage(image_path1, image_x_coordinate, 300, width=image_width1, height=image_width1 * .75)
-
-        c.drawString(x_coordinate4, 280, "Comparison Result")
-
-        c.drawImage(image_path2, image_x_coordinate, 80, width=image_width2, height=image_width2 * .75)
-
-        c.drawString(image_x_coordinate * 2 + 12, 65, "Unit Under Test")
-        c.drawString(((image_x_coordinate * 5) - 20) + 73, 65, "Reference Image")
+        text7 = 'Unit Under Test'
+        text_width7 = c.stringWidth(text7)
+        x_coordinate7 = (letter[0] - text_width7) / 2
+        c.drawString(x_coordinate7, 20, text7)
 
         # Save the canvas to the PDF file
         c.save()
 
-        print(f"Report generated, path: {output_filename}")
     else:
         pass
